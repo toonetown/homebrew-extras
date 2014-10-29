b09bcf82c9732d615d21b5da8ac7192e12885a6c
Nathan Toone <nathan@toonetown.com>
2014-10-29 16:01:49 -0600

    Update maven environment

--- Formula/maven-env.rb
+++ Formula/maven-env.rb
@@ -2,13 +2,13 @@ require 'pathname'
 require 'formula'
 
 class MavenEnv < Formula
-  version "1.0"
+  version "1.1"
   env_file = "10-maven-env"
   
   depends_on "toonetown-extras"
   homepage 'https://github.com/toonetown/homebrew-extras/'
   url "https://raw.githubusercontent.com/toonetown/homebrew-extras/master/etc/profile.d/#{env_file}"
-  sha1 "1edb5434071619c8226191976116039d094f3598"
+  sha1 "7d61e6fa5525e44990bb8b10ccd5e49578cfe4e0"
 
   def install
     (prefix+"etc/profile.d").install '10-maven-env'
