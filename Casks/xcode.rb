aefb8dddf7d7cef37d15fa98450e82c368899249
Nathan Toone <nathan@toonetown.com>
2014-10-30 15:34:06 -0600

    Add a cask for an separately-downloaded Xcode

---
+++ Casks/xcode.rb
@@ -0,0 +1,31 @@
+class Xcode < Cask
+  version '6.1'
+  sha256 'a5d2910faae2222975d0940dc9c6eabc56959b4ccf0c8505ac2cb011dbbda32f'
+
+  dmg_download_path=ENV['DMG_DOWNLOAD_PATH'] ? ENV['DMG_DOWNLOAD_PATH'] : "file:#{ENV['HOME']}/Downloads"
+  url "#{dmg_download_path}/56841_xcode_6.1.dmg"
+  homepage "http://developer.apple.com/"
+  license :unknown
+
+  app 'Xcode.app'
+  
+  postflight do
+    # Select this version of xcode
+    ohai "Selecting default version of Xcode - please enter your password"
+    system '/usr/bin/sudo', '-E', '--', '/usr/bin/xcode-select', '--switch', destination_path.join("Xcode.app")
+  end
+  
+  uninstall_postflight do
+    # Reset the version of xcode
+    ohai "Resetting default version of Xcode - please enter your password"
+    system '/usr/bin/sudo', '-E', '--', '/usr/bin/xcode-select', '--reset'
+  end
+  
+  caveats <<-EOS.undent
+    You must specify a location for the download loctaions by setting the
+    environment variable 'DMG_DOWNLOAD_PATH'.  This can be specified as a
+    local path (using the file:// scheme) or other URL, and will be used in 
+    the url for this cask. It defaults to ${HOME}/Downloads
+  EOS
+  
+end
