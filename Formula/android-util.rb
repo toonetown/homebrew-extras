be5c6b1f1c324b00a9c0ff45d2769c544def324a
Nathan Toone <nathan@toonetown.com>
2014-11-04 14:45:19 -0700

    Update android-util version

--- Formula/android-util.rb
+++ Formula/android-util.rb
@@ -1,7 +1,7 @@
 require 'formula'
 
 class AndroidUtil < Formula
-  version "1.0"
+  version "1.1"
   homepage 'https://github.com/toonetown/android-util/'
   url 'https://github.com/toonetown/android-util.git', :tag => "v#{version}"
   head 'https://github.com/toonetown/android-util.git', :branch => 'master'
