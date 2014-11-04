7a578133e041cf1d2106310726b0ad06ab26eb50
Nathan Toone <nathan@toonetown.com>
2014-11-04 16:32:40 -0700

    Bump version on android-util

--- Formula/android-util.rb
+++ Formula/android-util.rb
@@ -1,7 +1,7 @@
 require 'formula'
 
 class AndroidUtil < Formula
-  version "1.1"
+  version "1.2"
   homepage 'https://github.com/toonetown/android-util/'
   url 'https://github.com/toonetown/android-util.git', :tag => "v#{version}"
   head 'https://github.com/toonetown/android-util.git', :branch => 'master'
