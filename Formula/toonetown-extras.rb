ee701d44500507148fe4e25af2b198aca223394e
Nathan Toone <nathan@toonetown.com>
2014-11-05 11:37:45 -0700

    Add a brew-script command

--- Formula/toonetown-extras.rb
+++ Formula/toonetown-extras.rb
@@ -19,6 +19,7 @@ class ToonetownExtras < Formula
     prefix.install 'bin'
     (bin+'brewcask-outdated.rb').chmod 0755
     (bin+'brewcask-upgrade.rb').chmod 0755
+    (bin+'brew-script').chmod 0755
     
     ohai "Preparing profile directory - please enter your password"
     system '/bin/bash', 'script/prepare_profile', "#{HOMEBREW_PREFIX}"
