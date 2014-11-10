a883f61a9ba4b07cdaff87664366094c9a88038d
Nathan Toone <nathan@toonetown.com>
2014-11-10 11:06:55 -0700

    Add prl-tools formula

---
+++ Formula/prl-tools.rb
@@ -0,0 +1,20 @@
+require 'formula'
+
+class PrlTools < Formula
+  version "1.0"
+  homepage 'https://github.com/toonetown/prl-tools/'
+  url 'https://github.com/toonetown/prl-tools.git', :tag => "v#{version}"
+  head 'https://github.com/toonetown/prl-tools.git', :branch => 'master'
+  
+  skip_clean 'bin'
+  
+  def install
+    prefix.install 'bin'
+  end
+  
+  caveats <<-EOS.undent
+    These scripts depend on the prlctl command (which is installed by Paralles Desktop) existing in your path and 
+    Parallels Tools installed on the target VMs.  If you do not have parallels installed, you can install it using
+    'brew cask install parallels'
+  EOS
+end
