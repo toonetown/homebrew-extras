0112ceb28a661b43f8bc092ffc460696b1cffc71
Nathan Toone <nathan@toonetown.com>
2014-11-11 12:53:55 -0700

    Bump git-scripts version

---
+++ Formula/git-scripts.rb
@@ -0,0 +1,14 @@
+require 'formula'
+
+class GitScripts < Formula
+  version "1.1"
+  homepage 'https://github.com/toonetown/git-scripts/'
+  url 'https://github.com/toonetown/git-scripts.git', :tag => "v#{version}"
+  head 'https://github.com/toonetown/git-scripts.git', :branch => 'master'
+  
+  skip_clean 'bin'
+  
+  def install
+    prefix.install 'bin'
+  end
+end
