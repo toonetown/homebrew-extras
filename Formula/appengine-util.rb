bdb95932a6e0198990a94a38c98f08dfec61f9ac
Nathan Toone <nathan@toonetown.com>
2014-11-14 13:40:06 -0700

    Add appengine-util formula

---
+++ Formula/appengine-util.rb
@@ -0,0 +1,15 @@
+require 'formula'
+
+class AppengineUtil < Formula
+  version "1.0"
+  homepage 'https://github.com/toonetown/appengine-util/'
+  url 'https://github.com/toonetown/appengine-util.git', :tag => "v#{version}"
+  head 'https://github.com/toonetown/appengine-util.git', :branch => 'master'
+  depends_on "app-engine-java-sdk"
+  
+  skip_clean 'bin'
+  
+  def install
+    prefix.install 'bin'
+  end
+end
