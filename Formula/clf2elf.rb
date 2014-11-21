2a6d8ad546f828398fd0b416c388cf5047600055
Nathan Toone <nathan@toonetown.com>
2014-11-21 15:30:42 -0700

    Add formula for clf2elf

---
+++ Formula/clf2elf.rb
@@ -0,0 +1,16 @@
+require 'formula'
+
+class Clf2elf < Formula
+  version "1.0"
+  homepage 'https://github.com/toonetown/clf2elf/'
+  url 'https://github.com/toonetown/clf2elf.git', :tag => "v#{version}"
+  head 'https://github.com/toonetown/clf2elf.git', :branch => 'master'
+  depends_on "gawk"
+  
+  skip_clean 'bin'
+  
+  def install
+    prefix.install 'bin'
+    prefix.install 'share'
+  end
+end
