9a3397d9de7518d1d7871191aeff271e7cf6564b
Nathan Toone <nathan@toonetown.com>
2015-08-25 11:57:14 -0600

    Update android env to work with non-versioned NDK link

--- Formula/android-env.rb
+++ Formula/android-env.rb
@@ -2,13 +2,13 @@ require 'pathname'
 require 'formula'
 
 class AndroidEnv < Formula
-  version "1.2"
+  version "1.3"
   env_file = "20-android-env"
   
   depends_on "toonetown-extras"
   homepage 'https://github.com/toonetown/homebrew-extras/'
   url "https://raw.githubusercontent.com/toonetown/homebrew-extras/master/etc/profile.d/#{env_file}"
-  sha1 "31f022be03a9b843415beaba0c3a1c66ef8aa025"
+  sha1 "da434473d32e96c2d58823e3d7cbb9a3d0f3ceee"
 
   def install
     (prefix+"etc/profile.d").install '20-android-env'
