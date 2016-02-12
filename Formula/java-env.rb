5ceb3decdcb0db0979b1b790cef324307c883729
Nathan Toone <nathan@toonetown.com>
2016-02-12 09:05:40 -0700

    Default to java 1.8 - it's mostly stable now.

--- Formula/java-env.rb
+++ Formula/java-env.rb
@@ -2,13 +2,13 @@ require 'pathname'
 require 'formula'
 
 class JavaEnv < Formula
-  version "1.1"
+  version "1.2"
   env_file = "10-java-env"
   
   depends_on "toonetown-extras"
   homepage 'https://github.com/toonetown/homebrew-extras/'
   url "https://raw.githubusercontent.com/toonetown/homebrew-extras/master/etc/profile.d/#{env_file}"
-  sha1 "7f6e45ca45db465bd8e090dbfd6b6ca7f975fe7a"
+  sha1 "887e36285560e7e987e8ccf5bcec23b266d26ea0"
 
   def install
     (prefix+"etc/profile.d").install '10-java-env'
