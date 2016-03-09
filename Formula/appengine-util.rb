cb28cf2a51243c57d99475062e9a6e5ed55ad42c
Nathan Toone <nathan@toonetown.com>
2016-03-09 13:41:07 -0700

    Bump appengine-util version

--- Formula/appengine-util.rb
+++ Formula/appengine-util.rb
@@ -1,7 +1,7 @@
 require 'formula'
 
 class AppengineUtil < Formula
-  version "1.5"
+  version "1.6"
   homepage 'https://github.com/toonetown/appengine-util/'
   url 'https://github.com/toonetown/appengine-util.git', :tag => "v#{version}"
   head 'https://github.com/toonetown/appengine-util.git', :branch => 'master'
