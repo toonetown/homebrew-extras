6b2e568986d8eea6d525c74f16a6f680c8c52e0b
Nathan Toone <nathan@toonetown.com>
2016-04-14 14:56:10 -0600

    Add docker-port command to environment

--- Formula/docker-env.rb
+++ Formula/docker-env.rb
@@ -2,13 +2,13 @@ require 'pathname'
 require 'formula'
 
 class DockerEnv < Formula
-  version "1.14"
+  version "1.17"
   env_file = "20-docker-env"
   
   depends_on "toonetown-extras"
   homepage 'https://github.com/toonetown/homebrew-extras/'
   url "https://raw.githubusercontent.com/toonetown/homebrew-extras/master/etc/profile.d/#{env_file}"
-  sha256 "d3fa73afccb07dfbed6f0dde72917b402317912c852969855c5493e7d85297fa"
+  sha256 "72785907dfc42efff2853dba4d23cdd46035f6b916b575b9b388648906116efb"
 
   def install
     (prefix+"etc/profile.d").install '20-docker-env'
