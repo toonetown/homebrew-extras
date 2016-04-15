b92d195bf88b3263b4bcdbe26b2b842edbf6f0a5
Nathan Toone <nathan@toonetown.com>
2016-04-15 14:46:46 -0600

    Add some log management functions

--- Formula/docker-env.rb
+++ Formula/docker-env.rb
@@ -2,13 +2,13 @@ require 'pathname'
 require 'formula'
 
 class DockerEnv < Formula
-  version "1.17"
+  version "1.18"
   env_file = "20-docker-env"
   
   depends_on "toonetown-extras"
   homepage 'https://github.com/toonetown/homebrew-extras/'
   url "https://raw.githubusercontent.com/toonetown/homebrew-extras/master/etc/profile.d/#{env_file}"
-  sha256 "72785907dfc42efff2853dba4d23cdd46035f6b916b575b9b388648906116efb"
+  sha256 "9c482fe49b7d77ae473c2071053fa084755603cfa5d1a7cb1c90b13483194e74"
 
   def install
     (prefix+"etc/profile.d").install '20-docker-env'
