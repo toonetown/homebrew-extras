2567454ab51a6eda8b9b4f65632200612f199676
Nathan Toone <nathan@toonetown.com>
2016-05-06 09:35:27 -0600

    Update git-scripts (1.9)

--- Formula/git-scripts.rb
+++ Formula/git-scripts.rb
@@ -1,7 +1,7 @@
 require 'formula'
 
 class GitScripts < Formula
-  version "1.8"
+  version "1.9"
   homepage 'https://github.com/toonetown/git-scripts/'
   url 'https://github.com/toonetown/git-scripts.git', :tag => "v#{version}"
   head 'https://github.com/toonetown/git-scripts.git', :branch => 'master'
