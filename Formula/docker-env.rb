603c2bcb0acc284e7e744a0d239f31baa2a8ba3d
Nathan Toone <nathan@toonetown.com>
2016-05-26 10:55:49 -0600

    Update docker-env (1.23)

--- Formula/docker-env.rb
+++ Formula/docker-env.rb
@@ -2,13 +2,13 @@ require 'pathname'
 require 'formula'
 
 class DockerEnv < Formula
-  version "1.22"
+  version "1.23"
   env_file = "20-docker-env"
   
   depends_on "toonetown-extras"
   homepage 'https://github.com/toonetown/homebrew-extras/'
   url "https://raw.githubusercontent.com/toonetown/homebrew-extras/master/etc/profile.d/#{env_file}"
-  sha256 "a290d5c83bd83afd651b5f00434c87fdadee16d4c217dbe9d35c94adf9665edb"
+  sha256 "e85097319f0e26d35c64f0673b2d47750f963a090e8b9b05124b2a1df9ba938c"
 
   def install
     (prefix+"etc/profile.d").install '20-docker-env'
