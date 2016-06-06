878957e1f0b100115bb07b31a39f87ba98361c20
Nathan Toone <nathan@toonetown.com>
2016-06-06 08:41:27 -0600

    Update upgrade script so that it works again

--- lib/cask-upgrade.rb
+++ lib/cask-upgrade.rb
@@ -4,8 +4,8 @@ require Pathname(__FILE__).realpath.dirname.join('cask-outdated')
 class Hbc::Cask
   def upgrade
     ohai "Upgrading #{self}..."
-    inst = Hbc::Installer.new(self)
-    inst.uninstall(true)
-    inst.install(true)
+    inst = Hbc::Installer.new(self, force: true)
+    inst.uninstall
+    inst.install
   end
 end
