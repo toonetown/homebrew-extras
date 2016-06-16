bfb2fa6a7a2d6fadd5a7ec0eabcd6381ad21e565
Nathan Toone <nathan@toonetown.com>
2016-06-16 16:32:02 +0100

    Upgrade pretty-prompt (1.31)

--- Formula/pretty-prompt.rb
+++ Formula/pretty-prompt.rb
@@ -2,13 +2,13 @@ require 'pathname'
 require 'formula'
 
 class PrettyPrompt < Formula
-  version "1.30"
+  version "1.31"
   env_file = "01-pretty-prompt"
 
   depends_on "toonetown-extras"
   homepage 'https://github.com/toonetown/homebrew-extras/'
   url "https://raw.githubusercontent.com/toonetown/homebrew-extras/master/etc/profile.d/#{env_file}"
-  sha256 "cc9a49fe5e0d0c45efefac4441001ff32e0b25d57f4207d8d52fd6ce392a9091"
+  sha256 "29e6dea86dd17e8e2d6e964e9edec19521f6dbeb6da28bff972ed4de21b8d1bb"
 
   def install
     (prefix+"etc/profile.d").install '01-pretty-prompt'
