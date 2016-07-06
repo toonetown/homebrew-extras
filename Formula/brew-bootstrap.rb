c9994703ddafc9b4766e4b806375152e66d3ac50
Nathan Toone <nathan@toonetown.com>
2016-07-06 14:16:45 -0600

    Update to the latest version (with some minor fixes I've found)

--- Formula/brew-bootstrap.rb
+++ Formula/brew-bootstrap.rb
@@ -1,9 +1,9 @@
 require "formula"
 
 class BrewBootstrap < Formula
-  version "1.1"
+  version "1.2"
   homepage "https://gist.github.com/toonetown/48101686e509fda81335"
-  url "https://gist.github.com/48101686e509fda81335.git", :revision => "7982e69a"
+  url "https://gist.github.com/48101686e509fda81335.git", :revision => "84bd4233"
   head "https://gist.github.com/48101686e509fda81335.git", :branch => "master"
 
   skip_clean "bin"
