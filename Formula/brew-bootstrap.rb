b869be51daf3a7a6eb7e96c386b2bfe3e826ba75
Nathan Toone <nathan@toonetown.com>
2016-07-06 12:05:32 -0600

    Add brew-bootstrap (v1)

---
+++ Formula/brew-bootstrap.rb
@@ -0,0 +1,15 @@
+require "formula"
+
+class BrewBootstrap < Formula
+  version "1"
+  homepage "https://gist.github.com/toonetown/48101686e509fda81335"
+  url "https://gist.github.com/48101686e509fda81335.git", :revision => "67f70d68"
+  head "https://gist.github.com/48101686e509fda81335.git", :branch => "master"
+
+  skip_clean "bin"
+
+  def install
+    chmod 0755, "homebrew-bootstrap.sh"
+    bin.install "homebrew-bootstrap.sh" => "brew-bootstrap"
+  end
+end
