03c3729ebb44cd2965d7ea49fc8bc72fbe2e741d
Nathan Toone <nathan@toonetown.com>
2016-07-19 12:00:47 -0600

    Update java-env (1.35)

--- Formula/java-env.rb
+++ Formula/java-env.rb
@@ -4,6 +4,6 @@ require Pathname(__FILE__).realpath.dirname.join("../lib", "env-profile-formula"
 class JavaEnv < EnvProfileFormula
   desc "Sets the java environment and provides a `use-java` function"
   env_file "10-java-env"
-  version "1.15"
-  sha256 "a917023f84c9c0efefefa141df6bbac0cfb58723f0b22bd23441a9abf8e1e145"
+  version "1.35"
+  sha256 "c4203c8d13e13236e33fe73e59e43e0e318818cf494e87e598d2017547cc2952"
 end
