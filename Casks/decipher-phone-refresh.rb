465247f02b9c61c57def1765d5a280ace2b55158
Nathan Toone <nathan@toonetown.com>
2016-09-20 14:14:49 -0600

    Add decipher-phone-refresh (latest)

---
+++ Casks/decipher-phone-refresh.rb
@@ -0,0 +1,11 @@
+cask 'decipher-phone-refresh' do
+  version :latest
+  sha256 :no_check
+
+  url 'https://deciphertools.com/downloads/DecipherPhoneRefresh.dmg'
+  name 'Decipher Phone Refresh'
+  homepage 'https://deciphertools.com/decipher-phone-refresh.html'
+  license :commercial
+
+  app 'Decipher Phone Refresh.app'
+end
