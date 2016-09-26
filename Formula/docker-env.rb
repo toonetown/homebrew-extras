7c3468a14bf308b4f1b49c0dc37f35cb4f352cde
Nathan Toone <nathan@toonetown.com>
2016-09-26 17:15:02 -0600

    update docker-env (1.41)

--- Formula/docker-env.rb
+++ Formula/docker-env.rb
@@ -4,6 +4,6 @@ require Pathname(__FILE__).realpath.dirname.join("../lib", "env-profile-formula"
 class DockerEnv < EnvProfileFormula
   desc "Adds some commands to docker, docker-compose, and docker-machine"
   env_file "20-docker-env"
-  version "1.27"
-  sha256 "a6dc3199107c6afa88030ebe01c35ac8b2ae14bda71572b1e467eb8fc03e9799"
+  version "1.41"
+  sha256 "fca392c61374f75a9305980f9fe1ac537832ade475d32148e32fb74f2c39d6eb"
 end
