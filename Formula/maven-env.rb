307f756ed8ac361efa55cb05cb458115f2b79cbf
Nathan Toone <nathan@toonetown.com>
2016-09-26 17:15:40 -0600

    update maven-env (1.41)

--- Formula/maven-env.rb
+++ Formula/maven-env.rb
@@ -4,6 +4,6 @@ require Pathname(__FILE__).realpath.dirname.join("../lib", "env-profile-formula"
 class MavenEnv < EnvProfileFormula
   desc "Sets the values of M2_HOME and MAVEN_OPTS if maven is installed"
   env_file "10-maven-env"
-  version "1.35"
-  sha256 "cb442e100a7c5e9165c958d9597ec60877a864444c1c3594e6462020bb23170b"
+  version "1.41"
+  sha256 "f45a3278fe104bffb480d211566dec9e197164394a5fdd5e2613013862fd40c1"
 end
