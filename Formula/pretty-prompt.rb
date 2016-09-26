735f3a95030e160b0ae910f942b5779c4f6470a0
Nathan Toone <nathan@toonetown.com>
2016-09-26 17:15:51 -0600

    update pretty-prompt (1.41)

--- Formula/pretty-prompt.rb
+++ Formula/pretty-prompt.rb
@@ -4,6 +4,6 @@ require Pathname(__FILE__).realpath.dirname.join("../lib", "env-profile-formula"
 class PrettyPrompt < EnvProfileFormula
   desc "Sets colors of the prompt and loads scm-prompt if installed"
   env_file "01-pretty-prompt"
-  version "1.32"
-  sha256 "03320c035b80f9a8cf6778c9738dc3da9cb909374fd476d0a7d41af3f4f6c405"
+  version "1.41"
+  sha256 "fdfced6af45893ae16ea8d1d574cbd63aabd1e4460846d0cacda859d42db2a70"
 end
