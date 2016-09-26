ce5e678c9b51004a77984b247fce23e71631d78f
Nathan Toone <nathan@toonetown.com>
2016-09-26 17:15:19 -0600

    update google-cloud-sdk-env (1.41)

--- Formula/google-cloud-sdk-env.rb
+++ Formula/google-cloud-sdk-env.rb
@@ -4,6 +4,6 @@ require Pathname(__FILE__).realpath.dirname.join("../lib", "env-profile-formula"
 class GoogleCloudSdkEnv < EnvProfileFormula
   desc "Sets the value of GCLOUD_DIR"
   env_file "20-google-cloud-sdk-env"
-  version "1.37"
-  sha256 "da8acb4837eb31ddb468b1b0e2e4bb8340f7e72f1c54bd8f7152d8dc8878df5f"
+  version "1.41"
+  sha256 "eaf64513b59a0c7e3506a8284d6d171ed1bbdb7b8e0b1436cdbee5ceb5d85d94"
 end
