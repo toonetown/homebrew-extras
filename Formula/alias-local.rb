7d20e03809589bb0475ccd592d07bd66abe73674
Nathan Toone <nathan@toonetown.com>
2016-11-16 10:55:34 -0700

    update alias-local (2)

--- Formula/alias-local.rb
+++ Formula/alias-local.rb
@@ -5,6 +5,14 @@ class AliasLocal < GistFormula
   desc "Aliases localhost to an address"
   gist_hash "6015487b1ba11c69afad0b6429ecdd28"
   gist_file "alias-local"
-  gist_revision "2fc89c8a"
-  version "1"
+  gist_revision "46af8c59"
+  version "2"
+
+  def caveats; <<-EOS.undent
+    This script leverages sudo to work.  If you would like for this script to run without prompting for a password,
+    you can create a sudoers file by running 'sudo visudo -f /etc/sudoers.d/shimo-alias-local' and giving it the
+    following contents:
+      %admin  ALL=(root) NOPASSWD: /usr/local/bin/alias-local
+    EOS
+  end
 end
