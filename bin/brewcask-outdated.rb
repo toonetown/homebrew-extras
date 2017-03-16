d3e72ed58f33b89c6139fb03039116b91806f65e
Nathan Toone <nathan@toonetown.com>
2017-03-16 16:36:58 -0600

    Remove brew cask scripts (all are part of homebrew proper now - in better form)

--- bin/brewcask-outdated.rb
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env ruby
-require Pathname(__FILE__).realpath.dirname.join('../lib', 'cask-outdated')
-
-upgradable_casks = Hbc.upgradable
-
-puts upgradable_casks.length == 0 && 'No outdated packages' || upgradable_casks
