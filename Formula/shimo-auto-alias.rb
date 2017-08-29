143aaced188b25d8ca475a4e61d522258da41cd7
Nathan Toone <nathan@toonetown.com>
2017-08-29 08:06:28 -0600

    Update shimo-auto-alias (2)

--- Formula/shimo-auto-alias.rb
+++ Formula/shimo-auto-alias.rb
@@ -5,8 +5,8 @@ class ShimoAutoAlias < GistFormula
   desc "Automatically adds and removes Shimo SSH hosts"
   gist_hash "444211fc2830ae765693f8c28612b6e5"
   gist_file "shimo-auto-alias"
-  gist_revision "e998ff68"
-  version "1"
+  gist_revision "57a2a6b3"
+  version "2"
 
   depends_on "toonetown/extras/alias-local"
 end
