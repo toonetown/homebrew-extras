b777d890637757daf092bcdc2e60bf9efef75009
Nathan Toone <nathan@toonetown.com>
2017-10-17 15:54:36 -0600

    Added brewcask-check-appcast (1.0)

---
+++ Formula/brewcask-check-appcast.rb
@@ -0,0 +1,11 @@
+require "pathname"
+require Pathname(__FILE__).realpath.dirname.join("../lib", "gist-formula") unless defined?(gist_formula)
+
+class BrewcaskCheckAppcast < GistFormula
+  desc "Adds a 'check-appcast' action for brew-cask"
+  gist_hash "10e99bc35085a01851ab296e1e5f5379"
+  gist_file "brewcask-check-appcast"
+  install_name "brewcask-check-appcast"
+  gist_revision "be61887a"
+  version "1.0"
+end
