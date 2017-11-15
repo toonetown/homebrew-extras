8dad7fbe64f2ff9c96902598abcd4d4b71924df7
Nathan Toone <nathan@toonetown.com>
2017-11-15 15:21:14 -0700

    Update android-env (1.50)

--- Formula/android-env.rb
+++ Formula/android-env.rb
@@ -4,6 +4,6 @@ require Pathname(__FILE__).realpath.dirname.join("../lib", "env-profile-formula"
 class AndroidEnv < EnvProfileFormula
   desc "Sets ANDROID_HOME and ANDROID_NDK_HOME if packages are installed"
   env_file "20-android-env"
-  version "1.41"
-  sha256 "0bb68dddbec3dbed9184a25aa61692385ee25deb62512965ab3c252a95cf912d"
+  version "1.50"
+  sha256 "2a5708667e2f64382d2069f3e97eb04bba1bb079e7f0568afcf0208411f4d4a4"
 end
