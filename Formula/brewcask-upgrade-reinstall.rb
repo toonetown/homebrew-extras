7cac2c5cbb58f11221e32a74cfb5a96191d83e2f
Nathan Toone <nathan@toonetown.com>
2017-11-28 08:33:17 -0700

    Rename brewcask-upgrade (so it works with new upgrade version)

--- Formula/brewcask-upgrade-reinstall.rb
+++ Formula/brewcask-upgrade-reinstall.rb
@@ -1,11 +1,11 @@
 require "pathname"
 require Pathname(__FILE__).realpath.dirname.join("../lib", "gist-formula") unless defined?(gist_formula)
 
-class BrewcaskUpgrade < GistFormula
-  desc "Adds an 'upgrade' action for brew-cask"
+class BrewcaskUpgradeReinstall < GistFormula
+  desc "Adds a 'upgrade-reinstall' action for brew-cask"
   gist_hash "657606a8bac754eb2856f53e5001b4a3"
   gist_file "brewcask-upgrade"
-  install_name "brewcask-doupgrade"
+  install_name "brewcask-upgrade-reinstall"
   gist_revision "79f481de"
   version "1.1"
 end
