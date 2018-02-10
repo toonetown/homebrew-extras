f39ac1ea06333a23f0605a1f27276884428f1276
Nathan Toone <nathan@toonetown.com>
2018-02-10 16:09:38 -0700

    Update download location

--- Casks/kindle@1.17.rb
+++ Casks/kindle@1.17.rb
@@ -2,7 +2,7 @@ cask 'kindle@1.17' do
   version '44182'
   sha256 '28dc21246a9c7cdedd2d6f0f4082e6bf7ef9db9ce9d485548e8a9e1d19eae2ac'
 
-  url "http://kindleformac.amazon.com/#{version}/KindleForMac-#{version}.dmg"
+  url "http://www.toonetown.com/downloads/KindleForMac-#{version}.dmg"
   name 'Kindle for Mac'
   homepage 'https://www.amazon.com/gp/digital/fiona/kcp-landing-page'
 
