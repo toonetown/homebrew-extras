83deaa60e64cfc34da4c3dd02d51baacc7ceabf1
Nathan Toone <nathan@toonetown.com>
2018-07-31 12:34:53 -0600

    Add swup (1)

---
+++ Formula/swup.rb
@@ -0,0 +1,10 @@
+require "pathname"
+require Pathname(__FILE__).realpath.dirname.join("../lib", "gist-formula") unless defined?(gist_formula)
+
+class Swup < GistFormula
+  desc "Software updating script"
+  gist_hash "65b4ab33286c584fd678eceb23365cb1"
+  gist_file "swup"
+  gist_revision "16c04817"
+  version "1"
+end
