c834d4b869b816b8929c9b1f90fb4724378867eb
Nathan Toone <nathan@toonetown.com>
2018-08-07 15:33:05 -0600

    Update one-password-cli-env (1.55)

--- Formula/one-password-cli-env.rb
+++ Formula/one-password-cli-env.rb
@@ -4,6 +4,6 @@ require Pathname(__FILE__).realpath.dirname.join("../lib", "env-profile-formula"
 class OnePasswordCliEnv < EnvProfileFormula
   desc "Wraps the 'op' command with signin/signout helpers"
   env_file "20-1password-cli-env"
-  version "1.54"
-  sha256 "034ea95f9d266d6631e8afee3871d43e353ab647d5e01b214ed31dab5af21f45"
+  version "1.55"
+  sha256 "a018a6f7be96e17c1f8a2313681c06cb133ecac457ad3fcf4d020661c08bd2a4"
 end
