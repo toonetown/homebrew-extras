efb2fdfad96058ca79f218cdaf5a3e14e99aaa8d
Nathan Toone <nathan@toonetown.com>
2018-08-07 15:32:07 -0600

    Update bash-completion-env (1.55)

--- Formula/bash-completion-env.rb
+++ Formula/bash-completion-env.rb
@@ -4,6 +4,6 @@ require Pathname(__FILE__).realpath.dirname.join("../lib", "env-profile-formula"
 class BashCompletionEnv < EnvProfileFormula
   desc "Loads all the homebrew-installed bash completion scripts"
   env_file "10-bash-completion-env"
-  version "1.54"
-  sha256 "d76d6098586567d9fdd08dbfcb35ca50bac637d8184703ea6e87dd712a176383"
+  version "1.55"
+  sha256 "bc01ba7ce3fc7e7d4d25dc9cd08c9b8db2151342d83e70569da9015a29b495d1"
 end
