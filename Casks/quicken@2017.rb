12b35738b57b2acf526395da219f38b1479c3e97
Nathan Toone <nathan@toonetown.com>
2019-01-04 08:39:34 -0700

    Update quicken@2017 (48.20030.100)

--- Casks/quicken@2017.rb
+++ Casks/quicken@2017.rb
@@ -1,6 +1,6 @@
 cask 'quicken@2017' do
-  version '47.19730.100'
-  sha256 '8c2795940ff1a682badbc89ff90383eb9136ab7915bb861e8cbf93bd34553132'
+  version '48.20030.100'
+  sha256 '8e2820d5600c45a720be653594bf98c6acf397318d26dff25e183df61d3e9d7c'
 
   url "https://download.quicken.com/mac/Quicken2017/001/Release/88A2D0E2-FEA5-4251-A92E-6488AD26B7AC/Quicken2017-#{version}/Quicken2017-#{version}.zip"
   appcast 'https://download.quicken.com/mac/Quicken2017/001/Release/88A2D0E2-FEA5-4251-A92E-6488AD26B7AC/appcast.xml'
