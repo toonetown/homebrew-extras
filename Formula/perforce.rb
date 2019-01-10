367676891c5a947b41d35c6e6684cda0d10c43f3
Nathan Toone <nathan@toonetown.com>
2019-01-10 13:04:42 -0700

    Update perforce (r18.2)

--- Formula/perforce.rb
+++ Formula/perforce.rb
@@ -3,13 +3,8 @@ class Perforce < Formula
   homepage "http://www.perforce.com/"
   version "2016.1.1411799"
 
-  if MacOS.prefer_64_bit?
-    url "https://www.perforce.com/downloads/perforce/r16.1/bin.macosx105x86_64/p4"
-    sha256 "85153a60d75d84b172320e0d9348d0da67979f24a2e63650e5febf005afac778"
-  else
-    url "https://www.perforce.com/downloads/perforce/r16.1/bin.macosx105x86/p4"
-    sha256 "ddc3272687038b1b507017f6a702e9bdf41b8faf6d5b89ca27e0fc9b1879ad4f"
-  end
+  url "https://cdist2.perforce.com/perforce/r18.2/bin.macosx1010x86_64/p4"
+  sha256 "75635d11a92191154edf24796ed5d822f09160025af2f7e3ae915b7aa40d0b2c"
 
   bottle :unneeded
 
