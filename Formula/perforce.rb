5bb451d3c5626c50fdfd58b0d6489ca1c328fe70
Nathan Toone <nathan@toonetown.com>
2019-03-14 11:40:44 -0600

    Update perforce (r18.2)

--- Formula/perforce.rb
+++ Formula/perforce.rb
@@ -1,15 +1,15 @@
 class Perforce < Formula
   desc "Revision control system"
   homepage "http://www.perforce.com/"
-  version "2016.1.1411799"
+  version "2018.2.1751184"
 
   url "https://cdist2.perforce.com/perforce/r18.2/bin.macosx1010x86_64/p4"
-  sha256 "75635d11a92191154edf24796ed5d822f09160025af2f7e3ae915b7aa40d0b2c"
+  sha256 "fe327976f003ff2a2cc0c81c88e7b08a915609ad15a9c6cf343d885847100af0"
 
   bottle :unneeded
 
   def install
-    bin.install "p4"
+    bin.install "bin.macosx1010x86_64" => "p4"
   end
 
   test do
