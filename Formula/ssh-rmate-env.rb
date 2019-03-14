362f0129b4a1b820fb2f15884cb81dbd869508ea
Nathan Toone <nathan@toonetown.com>
2019-03-14 12:59:50 -0600

    Update ssh-rmate-env (1.57)

--- Formula/ssh-rmate-env.rb
+++ Formula/ssh-rmate-env.rb
@@ -4,6 +4,6 @@ require Pathname(__FILE__).realpath.dirname.join("../lib", "env-profile-formula"
 class SshRmateEnv < EnvProfileFormula
   desc "Aliases rmate to mate when connected to SSH"
   env_file "90-ssh-rmate-env"
-  version "1.38"
-  sha256 "80e9840956d4a5d79d80391d06a409763f6708a3407e1e5cd3a1fac15df71609"
+  version "1.57"
+  sha256 "19b4805a072b7e30c31672ef33857b308c3659984d941703dc1fb059e9fed990"
 end
