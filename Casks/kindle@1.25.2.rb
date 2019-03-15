30e722426a3bb3c22d467bb07e5342978f005d7e
Nathan Toone <nathan@toonetown.com>
2019-03-15 13:38:04 -0600

    Added kindle (1.25.2)

---
+++ Casks/kindle@1.25.2.rb
@@ -0,0 +1,23 @@
+cask 'kindle@1.25.2' do
+  version '52077'
+  sha256 'a069cd4cb4eee382ca7122b1652577b727851d244ffe75e60db08255b75f482b'
+
+  # s3.amazonaws.com/kindleformac was verified as official when first introduced to the cask
+  url "http://www.toonetown.com/downloads/KindleForMac-#{version}.dmg"
+  name 'Kindle for Mac'
+  homepage 'https://www.amazon.com/gp/digital/fiona/kcp-landing-page'
+
+  app 'Kindle.app'
+
+  postflight do
+    # Tweak renderer-test (see https://www.mobileread.com/forums/showthread.php?t=283371)
+    system_command '/bin/chmod', args: ['-x', "#{appdir}/Kindle.app/Contents/MacOS/renderer-test"]
+  end
+
+  zap trash: [
+               '~/Library/Preferences/com.amazon.Kindle.plist',
+               '~/Library/Application Support/Kindle/',
+               '~/Library/Saved Application State/com.amazon.Kindle.savedState/',
+               '~/Library/Caches/com.amazon.Kindle-Crash-Reporter/',
+             ]
+end
