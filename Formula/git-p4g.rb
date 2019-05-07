579780bfda643113df2adc3c4fc6eddc6a7b2cf3
Nathan Toone <nathan@toonetown.com>
2019-05-07 14:24:22 -0600

    Update git-p4g (2)

--- Formula/git-p4g.rb
+++ Formula/git-p4g.rb
@@ -5,8 +5,8 @@ class GitP4g < GistFormula
   desc "Adds perforce command line access using git configuration"
   gist_hash "5acae4666ecdf5535c1cff0dcd34a1c7"
   gist_file "git-p4g"
-  gist_revision "c9f5c484"
-  version "1"
+  gist_revision "35db68da"
+  version "2"
 
   depends_on "toonetown/extras/perforce"
 end
