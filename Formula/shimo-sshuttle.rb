8adc0098aa788ce38c66a91d92f672228a791966
Nathan Toone <nathan@toonetown.com>
2019-05-08 08:37:50 -0600

    Added shimo-sshuttle (4)

---
+++ Formula/shimo-sshuttle.rb
@@ -0,0 +1,20 @@
+require "pathname"
+require Pathname(__FILE__).realpath.dirname.join("../lib", "gist-formula") unless defined?(gist_formula)
+
+class ShimoSshuttle < GistFormula
+  desc "Connects to sshuttle tunnel using Shimo"
+  gist_hash "a0ad54becce8d7a9cbbea66ef7619ef7"
+  gist_file "shimo-sshuttle"
+  gist_revision "dea21ed8"
+  version "4"
+
+  depends_on "sshuttle"
+
+  def caveats; <<~EOS
+    This script leverages sudo to work.  If you would like for this script to run without prompting for a password,
+    you can create a sudoers file by running 'sudo visudo -f /etc/sudoers.d/shimo-sshuttle' and giving it the
+    following contents:
+      %admin  ALL=(root) NOPASSWD: /usr/local/opt/sshuttle/libexec/bin/python, /usr/local/bin/sshuttle
+  EOS
+  end
+end
