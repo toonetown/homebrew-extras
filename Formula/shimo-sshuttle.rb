77167b9ebec6c09904dbaf2a71f9ed74850da796
Nathan Toone <nathan@toonetown.com>
2019-05-24 08:51:48 -0600

    Update shimo-sshuttle (7)

--- Formula/shimo-sshuttle.rb
+++ Formula/shimo-sshuttle.rb
@@ -5,8 +5,8 @@ class ShimoSshuttle < GistFormula
   desc "Connects to sshuttle tunnel using Shimo"
   gist_hash "a0ad54becce8d7a9cbbea66ef7619ef7"
   gist_file "shimo-sshuttle"
-  gist_revision "44681ebd"
-  version "5"
+  gist_revision "6f6dee3f"
+  version "7"
 
   depends_on "sshuttle"
 
