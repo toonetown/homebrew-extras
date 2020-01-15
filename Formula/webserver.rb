f6621594faf66600d9350d460d3e392c6f07df08
Nathan Toone <nathan@toonetown.com>
2020-01-15 07:29:35 -0700

    Update webserver (3)

--- Formula/webserver.rb
+++ Formula/webserver.rb
@@ -5,6 +5,6 @@ class Webserver < GistFormula
   desc "Quick-and-dirty python web server"
   gist_hash "6603e97bb64c52fa9590"
   gist_file "webserver"
-  gist_revision "222a593a"
-  version "2"
+  gist_revision "21af6655"
+  version "3"
 end
