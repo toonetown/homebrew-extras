dc828e6abc86eb0e375f56458f5e446800f25f8e
Nathan Toone <nathan@toonetown.com>
2020-02-21 11:23:33 -0700

    Added print-cpp-tokens (1)

---
+++ Formula/print-cpp-tokens.rb
@@ -0,0 +1,10 @@
+require "pathname"
+require Pathname(__FILE__).realpath.dirname.join("../lib", "gist-formula") unless defined?(gist_formula)
+
+class PrintCppTokens < GistFormula
+  desc "Prints all the c++ tokens in a file"
+  gist_hash "b529408d508180dfee742322b086e962"
+  gist_file "print-cpp-tokens"
+  gist_revision "83c4c2c5"
+  version "1"
+end
