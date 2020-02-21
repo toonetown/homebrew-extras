88e075902e5e041fb97971bcc5ab4aae1adc82f4
Nathan Toone <nathan@toonetown.com>
2020-02-20 22:00:40 -0700

    Added logitech-harmony-remote (7.9.0)

---
+++ Casks/logitech-harmony-remote.rb
@@ -0,0 +1,25 @@
+cask 'logitech-harmony-remote' do
+  version '7.9.0'
+  sha256 '9605cf0d6398e013432647a62dd56a53ea3cd67d32165b8cf2d628ad01a337bf'
+
+  url "https://images-support.myharmony.com/software/LogitechHarmonyRemoteSoftware#{version}-OSX.dmg"
+  name 'Logitech Harmony Remote Software'
+  homepage 'https://support.myharmony.com/en-us/download'
+
+  pkg 'LogitechHarmonyRemoteSoftware.pkg'
+  pkg '.logitech-2.0.4a90.pkg'
+
+  preflight do
+    system_command '/usr/bin/ditto', 
+                   args: [ "#{staged_path}/.logitech-2.0.4a90.pkg", 
+                           '/Volumes/LogitechHarmonyRemoteSoftware/.logitech-2.0.4a90.pkg' ], 
+                   sudo: true
+  end
+
+  postflight do
+    system_command '/bin/rm', args: [ '-rf', '/Volumes/LogitechHarmonyRemoteSoftware' ], sudo: true
+  end
+
+  uninstall pkgutil: [ 'com.Belcarra.driver.LogitechHarmony',
+                       'com.logitech.harmony.cappuccino.client.logitech' ]
+end
