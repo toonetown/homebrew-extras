4c4b86eebffb07ee649df737825b5159c89999e6
Nathan Toone <nathan@toonetown.com>
2020-02-21 11:23:14 -0700

    Added csshx-env (1.62)

---
+++ Formula/csshx-env.rb
@@ -0,0 +1,9 @@
+require "pathname"
+require Pathname(__FILE__).realpath.dirname.join("../lib", "env-profile-formula") unless defined?(env_profile_formula)
+
+class CsshxEnv < EnvProfileFormula
+  desc "Sets the csshx autocomplete"
+  env_file "10-csshx-env"
+  version "1.62"
+  sha256 "a948decc3e79cc259db03b1ce41f46f169d4ea6bf02bf296b04793d33a70e42f"
+end
