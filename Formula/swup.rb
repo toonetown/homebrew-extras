82ff22e905ec03adde3ff9db14f247b5dce8c480
Nathan Toone <nathan@toonetown.com>
2020-02-21 10:13:47 -0700

    Update swup (14)

--- Formula/swup.rb
+++ Formula/swup.rb
@@ -5,6 +5,6 @@ class Swup < GistFormula
   desc "Software updating script"
   gist_hash "65b4ab33286c584fd678eceb23365cb1"
   gist_file "swup"
-  gist_revision "4bc1410c"
-  version "13"
+  gist_revision "ac7d4219"
+  version "14"
 end
