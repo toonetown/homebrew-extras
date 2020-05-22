ad0a7ae5d35256d0e09f525153ed4d01d8402c18
Nathan Toone <nathan@toonetown.com>
2020-05-22 11:21:46 -0600

    Update ksymbolicate (2)

--- Formula/ksymbolicate.rb
+++ Formula/ksymbolicate.rb
@@ -5,6 +5,6 @@ class Ksymbolicate < GistFormula
   desc "A script which will help decode macOS kernel panics"
   gist_hash "6f77d12a143655c8825f6a757d854674"
   gist_file "ksymbolicate"
-  gist_revision "ce3b999b"
-  version "1"
+  gist_revision "694e73fd"
+  version "2"
 end
