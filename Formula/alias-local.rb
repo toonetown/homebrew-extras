056b38ecb7809054492288a36ecaa9d718ccbd79
Nathan Toone <nathan@toonetown.com>
2020-07-08 16:09:46 -0600

    Fix typo in caveat

--- Formula/alias-local.rb
+++ Formula/alias-local.rb
@@ -10,7 +10,7 @@ class AliasLocal < GistFormula
 
   def caveats; <<~EOS
     This script leverages sudo to work.  If you would like for this script to run without prompting for a password,
-    you can create a sudoers file by running 'sudo visudo -f /etc/sudoers.d/shimo-alias-local' and giving it the
+    you can create a sudoers file by running 'sudo visudo -f /etc/sudoers.d/alias-local' and giving it the
     following contents:
       %admin  ALL=(root) NOPASSWD: /usr/local/bin/alias-local
   EOS
