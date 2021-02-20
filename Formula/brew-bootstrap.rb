11b2c8304071a1d7b052e57cd39d4af4efc8de78
Nathan Toone <nathan@toonetown.com>
2021-02-20 11:10:37 -0700

    Update brew-bootstrap (30)

--- Formula/brew-bootstrap.rb
+++ Formula/brew-bootstrap.rb
@@ -6,6 +6,6 @@ class BrewBootstrap < GistFormula
   gist_hash "48101686e509fda81335"
   gist_file "homebrew-bootstrap.sh"
   install_name "brew-bootstrap"
-  gist_revision "476b7101"
-  version "1.13"
+  gist_revision "df3d1d1c"
+  version "30"
 end
