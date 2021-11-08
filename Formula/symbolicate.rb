79a494d4405389849373867976952dae0cc10c01
Nathan Toone <nathan@toonetown.com>
2021-11-08 15:17:55 -0700

    Fix hashes for symbolicate

--- Formula/symbolicate.rb
+++ Formula/symbolicate.rb
@@ -3,8 +3,8 @@ require Pathname(__FILE__).realpath.dirname.join("../lib", "gist-formula") unles
 
 class Symbolicate < GistFormula
   desc "A script which will help decode macOS dumps, logs, and panics"
-  gist_hash "acf48a1729477add233e2eae36b936d6f8072ac9"
+  gist_hash "618507f37da728bbfe3f3e3490c8a550"
   gist_file "symbolicate"
-  gist_revision "87b85677"
+  gist_revision "acf48a17"
   version "2"
 end
