003845f52506b4ecb34f73f9f2e8df0531d507f6
Nathan Toone <nathan@toonetown.com>
2021-11-08 15:12:25 -0700

    Update symbolicate (2)

--- Formula/symbolicate.rb
+++ Formula/symbolicate.rb
@@ -3,8 +3,8 @@ require Pathname(__FILE__).realpath.dirname.join("../lib", "gist-formula") unles
 
 class Symbolicate < GistFormula
   desc "A script which will help decode macOS dumps, logs, and panics"
-  gist_hash "618507f37da728bbfe3f3e3490c8a550"
+  gist_hash "acf48a1729477add233e2eae36b936d6f8072ac9"
   gist_file "symbolicate"
   gist_revision "87b85677"
-  version "1"
+  version "2"
 end
