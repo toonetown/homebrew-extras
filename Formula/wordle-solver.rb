6dc4643c3f192fadba94b8b3d789546551e0a42b
Nathan Toone <nathan@toonetown.com>
2022-02-07 17:39:19 -0700

    Add wordle-solver (1)

---
+++ Formula/wordle-solver.rb
@@ -0,0 +1,10 @@
+require "pathname"
+require Pathname(__FILE__).realpath.dirname.join("../lib", "gist-formula") unless defined?(gist_formula)
+
+class WordleSolver < GistFormula
+  desc "Helps solve wordle puzzles"
+  gist_hash "5b7e8c8dc47496f65d2468bc13ab507a"
+  gist_file "wordle-solver"
+  gist_revision "7d17119c"
+  version "1"
+end
