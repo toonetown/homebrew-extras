eb72c29606fc23df1bd2114f40c0dcedf2a0ed8a
Nathan Toone <nathan@toonetown.com>
2022-08-08 16:04:19 -0600

    Add ips2crash (head only)

---
+++ Formula/ips2crash.rb
@@ -0,0 +1,16 @@
+class Ips2crash < Formula
+  desc "A macOS command line too to convert a .ips file to a legacy .crash log file"
+  homepage "https://github.com/packagesdev/ips2crash"
+  head "https://github.com/packagesdev/ips2crash.git"
+
+  depends_on :xcode => :build
+
+  def install
+    xcodebuild "-project", "tool_ips2crash/ips2crash.xcodeproj", "SYMROOT=build"
+    bin.install "./tool_ips2crash/build/Release/ips2crash"
+  end
+
+  test do
+    system true
+  end
+end
