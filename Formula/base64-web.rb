4547ed269829785ecee95e31e580bee63fbbe7dd
Nathan Toone <nathan@toonetown.com>
2023-05-30 10:46:14 -0600

    Updated base64-web (2)

--- Formula/base64-web.rb
+++ Formula/base64-web.rb
@@ -5,6 +5,6 @@ class Base64Web < GistFormula
   desc "Adds an option to encode/decode url-safe base64"
   gist_hash "90b65f630ff51f570ff893b3637c5b17"
   gist_file "base64"
-  gist_revision "daa198ad"
-  version "1"
+  gist_revision "122282fb"
+  version "2"
 end
