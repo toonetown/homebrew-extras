89f51755ca4446e5c58ea3eba186fff2f93d0096
Nathan Toone <nathan@toonetown.com>
2024-11-12 10:06:18 -0700

    Use versioned download of wssa-diag

--- Formula/wssa-diag.rb
+++ Formula/wssa-diag.rb
@@ -1,9 +1,10 @@
 class WssaDiag < Formula
-  desc "Shell script to gather debugging, troubleshooting, and trace log information for Symantec WSSA"
-  homepage "https://techdocs.broadcom.com/bin/gethidpage?ux-context-string=wssa-macdiags&appid=wss&language=&format=rendered"
-  url "https://techdocs.broadcom.com/content/dam/broadcom/techdocs/us/en/dita/symantec-security-software/web-and-network-security/web-security-service/content/wssa-macos-diags.zip"
   version "1.35"
   sha256 "146fe459db312681a0e51817246cb8c48fd2ad95e6e2d1791ad0e95538844396"
+
+  url "https://techdocs.broadcom.com/content/dam/broadcom/techdocs/us/en/dita/symantec-security-software/web-and-network-security/web-security-service/content/wssa-macos-diags-#{version}.zip"
+  desc "Shell script to gather debugging, troubleshooting, and trace log information for Symantec WSSA"
+  homepage "https://techdocs.broadcom.com/bin/gethidpage?ux-context-string=wssa-macdiags&appid=wss&language=&format=rendered"
   license "Commercial"
 
   def install
