0ae7d4b5d37b1815cf736c0499aca1a7a4b16973
Nathan Toone <nathan@toonetown.com>
2024-11-25 13:06:47 -0700

    Updated xsymbolicate (10)

--- Casks/xsymbolicate.rb
+++ Casks/xsymbolicate.rb
@@ -2,8 +2,8 @@ require 'pathname'
 require Pathname(@path).realpath.dirname.join('../lib', 'predownloaded-cask') unless defined?(predownloaded_cask)
 
 cask 'xsymbolicate' do
-  version '8'
-  sha256 '2f47e38a688a3fc905348903439c30708c8accba02f81250c526f5954afbdb56'
+  version '10'
+  sha256 'fddfe6e8ba5ec041ccad233ce44061e5f511d78daff1cba20b7559d5a1b3e298'
 
   url cask_url("Xsymbolicate_#{version}.zip")
   name 'Xsymbolicate'
