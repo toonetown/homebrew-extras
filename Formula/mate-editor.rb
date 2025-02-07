07c96ce714af61c54d92b44afb86278c90df5733
Nathan Toone <nathan@toonetown.com>
2025-02-07 12:32:59 -0700

    Update mate-editor (5)

--- Formula/mate-editor.rb
+++ Formula/mate-editor.rb
@@ -5,8 +5,8 @@ class MateEditor < GistFormula
   desc "Mate wraper for EDITOR variable"
   gist_hash "bb6bc9e8476b1a591dd25faa7e180acb"
   gist_file "mate-editor"
-  gist_revision "9676575a"
-  version "3"
+  gist_revision "82aa8e52"
+  version "5"
 
   homepage "https://gist.github.com/toonetown/#{gist_hash}"
   url "https://gist.github.com/#{gist_hash}.git", :revision => gist_revision
