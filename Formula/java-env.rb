4161e6b406ee5c5fe4885bbf98c98f4a5c04f083
Nathan Toone <nathan@toonetown.com>
2025-02-07 13:10:23 -0700

    Update java-env (1.65)

--- Formula/java-env.rb
+++ Formula/java-env.rb
@@ -4,8 +4,8 @@ require Pathname(__FILE__).realpath.dirname.join("../lib", "env-profile-formula"
 class JavaEnv < EnvProfileFormula
   desc "Sets the java environment and provides a `use-java` function"
   env_file "10-java-env"
-  version "1.61"
-  sha256 "18697d80c98c6b7959f2ac325f202990d5ebaf8f82fcb0bf9ffd36ac905175ee"
+  version "1.65"
+  sha256 "27b66134f0c8f01af8153c52c4cd3a5e2624d4ab786cc3e0ba771349065e13bc"
 
   url "https://raw.githubusercontent.com/toonetown/homebrew-extras/master/etc/profile.d/#{env_file}"
   homepage "https://github.com/toonetown/homebrew-extras/"
