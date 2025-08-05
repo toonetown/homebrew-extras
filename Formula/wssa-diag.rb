07d0cc7a6458b7275c4de7784ad09142e18fd1e2
Nathan Toone <nathan@toonetown.com>
2025-08-05 08:26:26 -0600

    Updated wssa-diag (1.40)

--- Formula/wssa-diag.rb
+++ Formula/wssa-diag.rb
@@ -1,6 +1,6 @@
 class WssaDiag < Formula
-  version "1.39"
-  sha256 "5e938505a66ab197a188dd72f088876557ecaa46bb5e7f1c2c7db68da8815c3a"
+  version "1.40"
+  sha256 "0a0c2f9d19c21ee10089db92e523dd23acc26cca88865014bc08c0531b4a2c1a"
 
   url "https://techdocs.broadcom.com/content/dam/broadcom/techdocs/us/en/dita/symantec-security-software/web-and-network-security/web-security-service/content/wssa-macos-diags-#{version}.zip"
   desc "Shell script to gather debugging, troubleshooting, and trace log information for Symantec WSSA"
